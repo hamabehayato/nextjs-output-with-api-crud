bffcbaa779733c300211721a7d6ce37fabca1ee2
hamabehayato <be_dash1122@icloud.com>
2023-12-03 13:16:41 +0900

    誤字修正

--- api-server/src/migration/1700394238770-Todo.ts
+++ api-server/src/migration/1700394238770-Todo.ts
@@ -1,12 +1,10 @@
-// ファイル生成コマンド npx typeorm migration:create type-orm/migration/Todo
+// ファイル生成コマンド npx typeorm migration:create src/migration/User
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateTodoTable1700394238770 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     /* todoテーブルがない場合は todo テーブル作成 */
-    const tableExists = await queryRunner.hasTable('todo');
-    if (!tableExists) {
-      await queryRunner.query(`
+    await queryRunner.query(`
         CREATE TABLE \`todo\` (
           \`id\` INT NOT NULL AUTO_INCREMENT,
           \`title\` VARCHAR(191) NOT NULL,
@@ -14,21 +12,6 @@ export class CreateTodoTable1700394238770 implements MigrationInterface {
           PRIMARY KEY (\`id\`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
       `);
-    }
-
-    /* todo テーブルが空の場合のみ初期データを投入する */
-    const rowCount = await queryRunner.query(`
-      SELECT COUNT(*) as count FROM \`todo\`;
-    `);
-
-    console.log('rowCount', rowCount);
-    if (rowCount[0].count === 0) {
-      await queryRunner.query(`
-        INSERT INTO \`todo\` (\`title\`, \`content\`) VALUES
-          ('Todo 1', 'Content 1'),
-          ('Todo 2', 'Content 2');
-      `);
-    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
